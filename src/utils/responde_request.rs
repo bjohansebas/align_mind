076852257207889257fecbd103e83c23d49345d7
Sebastian Beltran <bjohansebas@gmail.com>
2023-02-19 21:13:18 -0500

    Add response api

--- src/utils/responde_request.rs
+++ src/utils/responde_request.rs
@@ -1,45 +1,44 @@
-use align_mind_server::models::response_model::{Response, ResponseError, ResponseSuccess};
+use align_mind_server::models::response_model::{ResponseMessage, ResponseValue};
 use rocket::{http::Status, response::status, serde::json::Json};
 use serde::Serialize;
+use serde_json::Value;
 
-pub fn response_api(
-    entity: Result<ResponseSuccess, ResponseError>,
-) -> status::Custom<Json<Response>> {
-    match entity {
-        Ok(response) => status::Custom(
-            Status::from_code(Status::Ok.code).unwrap(),
-            Json(Response {
-                message: response.message,
-                data: serde_json::to_value(response.data).unwrap(),
+pub fn response_message_api(entity: ResponseMessage) -> status::Custom<Json<ResponseMessage>> {
+    match entity.code {
+        Some(code) => status::Custom(
+            Status::from_code(code).unwrap(),
+            Json(ResponseMessage {
+                message: entity.message,
+                code: Some(code),
             }),
         ),
-        Err(response) => status::Custom(
-            Status::from_code(response.code).unwrap(),
-            Json(Response {
-                message: response.message,
-                data: serde_json::to_value("").unwrap(),
+        None => status::Custom(
+            Status::from_code(Status::Accepted.code).unwrap(),
+            Json(ResponseMessage {
+                message: entity.message,
+                code: None,
             }),
         ),
     }
 }
 
-pub fn response_api_data<T: Serialize>(
-    entity: Result<T, ResponseError>,
-) -> status::Custom<Json<Response>> {
+pub fn response_value_api<T: Serialize>(
+    entity: Result<ResponseValue<T>, ResponseMessage>,
+) -> status::Custom<Json<Value>> {
     match entity {
         Ok(response) => status::Custom(
-            Status::from_code(Status::Ok.code).unwrap(),
-            Json(Response {
-                message: "Ok".to_string(),
-                data: serde_json::to_value(response).unwrap(),
-            }),
+            Status::from_code(response.code).unwrap(),
+            Json(serde_json::to_value(response.value).unwrap()),
         ),
         Err(response) => status::Custom(
-            Status::from_code(response.code).unwrap(),
-            Json(Response {
-                message: response.message,
-                data: serde_json::to_value("").unwrap(),
-            }),
+            Status::from_code(response.code.unwrap()).unwrap(),
+            Json(
+                serde_json::to_value(ResponseMessage {
+                    code: response.code,
+                    message: response.message,
+                })
+                .unwrap(),
+            ),
         ),
     }
 }
